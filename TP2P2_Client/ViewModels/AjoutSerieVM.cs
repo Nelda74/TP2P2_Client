00e7db7a0da15bfea990da057892fb55b5c4c003
cherifad <cherifad@D250-S09>
2023-02-06 11:46:21 +0100

    controls

--- TP2P2_Client/ViewModels/AjoutSerieVM.cs
+++ TP2P2_Client/ViewModels/AjoutSerieVM.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TP2P2_Client.Models;
+using TP2P2_Client.Service;
 
 namespace TP2P2_Client.ViewModels
 {
@@ -12,6 +14,61 @@ namespace TP2P2_Client.ViewModels
         public AjoutSerieVM() 
         {
 
-        }           
+        }
+
+        private String titre;
+
+        public String Titre
+        {
+            get { return titre; }
+            set { titre = value; }
+        }
+
+        private String resume;
+
+        public String Resume
+        {
+            get { return resume; }
+            set { resume = value; }
+        }
+
+        private int nbSaisons;
+
+        public int NbSaisons
+        {
+            get { return nbSaisons; }
+            set { nbSaisons = value; }
+        }
+
+        private int nbEpisodes;
+
+        public int NbEpisodes
+        {
+            get { return nbEpisodes; }
+            set { nbEpisodes = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private String chaine;
+
+        public String Chaine
+        {
+            get { return chaine; }
+            set { chaine = value; }
+        }
+
+        public void AjoutSerie()
+        {
+            WSService service = new WSService("https://apiservicecherad.azurewebsites.net");
+
+            service.PostSerieAsync(new Serie(Titre, Resume, NbSaisons, NbEpisodes, Year, Chaine));
+        }
     }
 }
